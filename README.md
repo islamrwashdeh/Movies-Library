3a54e5cabeb0dc3dadf800a1a55116c6fec08558
sal14ma <islamrwashdeh@yahoo.com>
2022-05-07 15:22:59 +0300

    update

--- README.md
+++ README.md
@@ -23,11 +23,24 @@
 
 6. app.listen(PORT, ()=>{ console.log("Anything") })
 
-7. I can create end points (ex: app.get("/", name of the function))
-
-
+7. I can create end points (ex: app.get("/", name of the function))           
 ## Project Features
 1.  retrieve specific information from my data.json file.
 2.  send back that information to the homepage or "/".
 3.  return a welcoming string on the favorite page "/favorite".
 4.  format the retrieved data using a constructor into the form I want.
+
+# movies-library 0.2
+## WRRC
+![WRRC](https://github.com/islamrwashdeh/Movies-Library/blob/main/Screenshot%20(76)%20(1).png?raw=true)
+1. install axios
+
+2. create .env file to save api key inside it
+
+3. take API from TMDB wepsite and clone it .
+
+4. create new tow endpoints (trending , search)
+## Project Features
+1.  Getting the latest trending movie
+2. Search for movies by name
+
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-'user strict'
 const { response } = require("express");
 const express= require("express");
 const movies =require("./data.json");
 const app =express();
 
-function Movie(title, overview, poster_path){
+
+const axios =require("axios");
+
+// read dotenv file
+const dotenv =require("dotenv");
+
+//start (configer) the dotenv
+dotenv.config();
+//process.env >> built in fucn
+const APIKEY =process.env.APIKEY;
+
+
+function Movie(title, overview, poster_path,id,release_date){
     this.title=title;
     this.overview=overview;
     this.poster_path=poster_path;
+    this.id=id;
+    this.release_date=release_date;
 }
 
 app.get("/",helloMovie);
 app.get("/favorit",hellofavorit);
+app.get("/trending",helloTrending);
+app.get("/search",hellosearch);
 app.get("*",notFoundHandler);
 
 app.use(errorHandler);
@@ -25,6 +40,9 @@ function errorHandler(error,req,res){
 
 function helloMovie(req ,res){
     let arr=[]
+    // arr.push(movies.title);
+    // arr.push(movies.overview);
+    // arr.push(movies.poster_path);
     movies.data.forEach((value) => {
         let theMovie = new Movie(value.title, value.overview, value.poster_path);
         arr.push(theMovie);
@@ -39,6 +57,39 @@ function hellofavorit(req,res){
  .catch(error =>{
     errorHandler("Sorry, something went wrong",req,res);
 })
+}
+
+function helloTrending(req,res){
+    let result = [];
+    
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+   .then(apiResponse =>{
+       apiResponse.data.results.map(value =>{ 
+           let oneMoveDetails =new Movie(value.id,value.title,value.release_date,value.overview,value.poster_path)
+           result.push(oneMoveDetails);
+       })
+       return res.status(200).json(result);
+  }).catch(error =>{
+      errorHandler("Sorry, something went wrong",req,res);
+  })
+}
+
+function hellosearch(req,res){
+   const search ="The Royal Treatment,";
+    let Sresult = [];
+    
+    axios.get(` https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${search}`)
+   .then(apiResponse =>{
+       apiResponse.data.results.map(value =>{ 
+           let oneMoveSearch =new Movie(value.title || "A/N" ,value.overview || "A/N" ,value.release_date || "A/N" ,value.id || "A/N" ,value.release_date || "A/N")
+           Sresult.push(oneMoveSearch);
+       })
+       return res.status(200).json(Sresult);
+  }).catch(error =>{
+      errorHandler("Sorry, something went wrong",req,res);
+  })
+
+  
 }
 
 function notFoundHandler(req,res){
